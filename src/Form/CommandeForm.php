6c8a80d20df8f41902d399057a8c3e4cc5e1d0c4
Yoann <yoyolc.2004@gmail.com>
2025-06-26 05:01:08 +0200

    ajout CRUD commandes

---
+++ src/Form/CommandeForm.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commande;
+use App\Entity\Lunette;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommandeForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('valide')
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'id',
+            ])
+            ->add('lunettes', EntityType::class, [
+                'class' => Lunette::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commande::class,
+        ]);
+    }
+}
